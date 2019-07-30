449caf3b01288a570cdd14c6b3da6092156eb9da
Brandon Hong <Brandon@Nicholes-iPhone.localdomain>
2019-07-30 10:45:57 -0700

    update searchbar on user selection and improved styling

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const list = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'San Francisco', 'Columbus',
                 'Fort Worth', 'Indianapolis', 'Charlotte', 'Seattle', 'Denver', 'Washington D.C.', 'Boston', 'El Paso', 'Detroit', 'Oklahoma City', 'Las Vegas', 'Louisville', 'Baltimore', 'Milwaukee',
                 'Albuquerque', 'Tucson', 'Fresno', 'Sacramento', 'Mesa', 'Kansas City', 'Atlanta', 'Long Beach', 'Omaha', 'Raleigh', 'Colorado Springs', 'Miami', 'Virginia Beach', 'Oakland',
-                'Minneapolis', 'Tulsa', 'Arlington', 'New Orleans'];
+                'Minneapolis', 'Tulsa', 'Arlington', 'New Orleans', 'Wichita', 'Cleveland', 'Tampa', 'Bakersfield', 'Aurora', 'Anaheim', 'Honolulu', 'Santa Ana', 'Riverside', 'Corpus Christi',
+                'Lexington', 'Stockton', 'St. Louis', 'Saint Paul', 'Henderson', 'Pittsburgh', 'Cincinnati', 'Anchorage', 'Greensboro', 'Plano', 'Newark', 'Lincoln', 'Orlando', 'Irvine', 'Toledo',
+                'Jersey City', 'Chula Vista', 'Durham', 'Fort Wayne', 'St. Petersburg', 'Laredo', 'Buffalo', 'Madison', 'Lubbock', 'Chandler', 'Scottsdale', 'Reno', 'Glendale', 'Norfolk',
+                'Winston-Salem', 'North Las Vegas', 'Gilbert', 'Chesapeake', 'Irving', 'Hialeah', 'Garland', 'Fremont', 'Richmond', 'Boise', 'Baton Rouge', 'Des Moines'];
 const searchBar = document.getElementById('typeahead');
+const selection = document.getElementById('suggestions');
+const option = document.querySelectorAll('li');
+
 var suggestions = [];
 
 searchBar.addEventListener('input', (event) => {
@@ -11,8 +17,14 @@ searchBar.addEventListener('input', (event) => {
         const regex = new RegExp(`^${input}`, 'i');
         suggestions = list.sort().filter(v => regex.test(v));
     } else {
-        suggestions = []
+        suggestions = [];
     }
+    displaySuggestions();
+});
+
+selection.addEventListener('click', (event) => {
+    searchBar.value = event.target.textContent;
+    suggestions = [];
     displaySuggestions();
 });
 
