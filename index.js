576c87b106d82fb65e71c065688cae1aee7b6830
Brandon Hong <Brandon@Nicholes-iPhone.localdomain>
2019-07-30 15:18:18 -0700

    use arrow keys to navigate suggestions instead of tab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const list = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'San Francisco', 'Columbus',
+const statesList = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'San Francisco', 'Columbus',
                 'Fort Worth', 'Indianapolis', 'Charlotte', 'Seattle', 'Denver', 'Washington D.C.', 'Boston', 'El Paso', 'Detroit', 'Oklahoma City', 'Las Vegas', 'Louisville', 'Baltimore', 'Milwaukee',
                 'Albuquerque', 'Tucson', 'Fresno', 'Sacramento', 'Mesa', 'Kansas City', 'Atlanta', 'Long Beach', 'Omaha', 'Raleigh', 'Colorado Springs', 'Miami', 'Virginia Beach', 'Oakland',
                 'Minneapolis', 'Tulsa', 'Arlington', 'New Orleans', 'Wichita', 'Cleveland', 'Tampa', 'Bakersfield', 'Aurora', 'Anaheim', 'Honolulu', 'Santa Ana', 'Riverside', 'Corpus Christi',
@@ -8,19 +8,22 @@ const list = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Phila
 const searchBar = document.getElementById('typeahead');
 const suggestionDisplay = document.getElementById('suggestions');
 
-var suggestions = [];
-var firstOption;
+let suggestions;
+let HTMLSuggestions;
+let firstOption;
 
 searchBar.addEventListener('input', (event) => {
     const input = event.target.value;
-    if(input.length > 0) {
+    if (input.length > 0) {
         const regex = new RegExp(`^${input}`, 'i');
-        suggestions = list.sort().filter(v => regex.test(v));
+        suggestions = statesList.sort().filter(word => regex.test(word));
     } else {
         suggestions = [];
     }
     displaySuggestions();
-    firstOption = document.querySelector('li');
+    HTMLSuggestions = Array.from(document.querySelectorAll('li.suggestion'));
+    firstOption = HTMLSuggestions[0];
+    lastOption = HTMLSuggestions[HTMLSuggestions.length - 1];
     toggleHighlight(firstOption)
 });
 
@@ -47,11 +50,14 @@ suggestionDisplay.addEventListener('mouseout', (event) => {
 
 searchBar.addEventListener('keydown', (event) => {
     previousOption = document.getElementById('highlight');
-    if (event.keyCode === 9) {
+    if (event.keyCode === 40) {
         event.preventDefault();
         toggleHighlight(previousOption);
-        previousOption.id = null;
         previousOption.nextSibling !== null ? toggleHighlight(previousOption.nextSibling) : toggleHighlight(firstOption);
+    } else if (event.keyCode === 38) {
+        event.preventDefault();
+        toggleHighlight(previousOption);
+        previousOption.previousSibling !== null ? toggleHighlight(previousOption.previousSibling) : toggleHighlight(lastOption);
     } else if (event.keyCode === 13) {
         event.preventDefault();
         searchBar.value = previousOption.textContent;
@@ -59,7 +65,7 @@ searchBar.addEventListener('keydown', (event) => {
 });
 
 function displaySuggestions() {
-    document.getElementById('suggestions').innerHTML = (suggestions.map(element => '<li>'+element+'</li>')).join('');
+    document.getElementById('suggestions').innerHTML = (suggestions.map(element => '<li class=\'suggestion\'>'+element+'</li>')).join('');
 }
 
 function toggleHighlight(HTMLElement) {
