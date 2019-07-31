02969cdd8c02a6e2db94440ae1862d4599be5521
Brandon Hong <Brandon@Nicholes-iPhone.localdomain>
2019-07-30 17:05:26 -0700

    abstracted typeahead.js to allow for client use

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# typeahead
+# To use typeahead.js:
+# 1. Download typeahead.js and add it to your project folder
+# 2. Add '<script src="typeahead.js"></script>' to your HTML file following the </body> tag
+# 3. Specify where you want the typeahead element to be rendered'<script>new Typeahead("ENTER HTML ELEMENT ID")</script>'
---
+++ typeahead.js
@@ -0,0 +1,84 @@
+const statesList = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'San Francisco', 'Columbus',
+                'Fort Worth', 'Indianapolis', 'Charlotte', 'Seattle', 'Denver', 'Washington D.C.', 'Boston', 'El Paso', 'Detroit', 'Oklahoma City', 'Las Vegas', 'Louisville', 'Baltimore', 'Milwaukee',
+                'Albuquerque', 'Tucson', 'Fresno', 'Sacramento', 'Mesa', 'Kansas City', 'Atlanta', 'Long Beach', 'Omaha', 'Raleigh', 'Colorado Springs', 'Miami', 'Virginia Beach', 'Oakland',
+                'Minneapolis', 'Tulsa', 'Arlington', 'New Orleans', 'Wichita', 'Cleveland', 'Tampa', 'Bakersfield', 'Aurora', 'Anaheim', 'Honolulu', 'Santa Ana', 'Riverside', 'Corpus Christi',
+                'Lexington', 'Stockton', 'St. Louis', 'Saint Paul', 'Henderson', 'Pittsburgh', 'Cincinnati', 'Anchorage', 'Greensboro', 'Plano', 'Newark', 'Lincoln', 'Orlando', 'Irvine', 'Toledo',
+                'Jersey City', 'Chula Vista', 'Durham', 'Fort Wayne', 'St. Petersburg', 'Laredo', 'Buffalo', 'Madison', 'Lubbock', 'Chandler', 'Scottsdale', 'Reno', 'Glendale', 'Norfolk',
+                'Winston-Salem', 'North Las Vegas', 'Gilbert', 'Chesapeake', 'Irving', 'Hialeah', 'Garland', 'Fremont', 'Richmond', 'Boise', 'Baton Rouge', 'Des Moines'];
+let suggestions;
+
+class Typeahead {
+    constructor(containerID) {
+        document.getElementById(containerID).innerHTML = '<input id=\'typeahead\' type=\'text\'></input><ul id=\'suggestions\'></ul>';
+        addEventListeners();
+    }
+}
+
+function addEventListeners() {
+    const searchBar = document.getElementById('typeahead');
+    const suggestionDisplay = document.getElementById('suggestions');
+
+    let HTMLSuggestions;
+    let firstOption;
+    let lastOption;
+
+    searchBar.addEventListener('input', (event) => {
+        const input = event.target.value;
+        if (input.length > 0) {
+            const regex = new RegExp(`^${input}`, 'i');
+            suggestions = statesList.sort().filter(word => regex.test(word));
+        } else {
+            suggestions = [];
+        }
+        displaySuggestions();
+        HTMLSuggestions = Array.from(document.querySelectorAll('li.suggestion'));
+        firstOption = HTMLSuggestions[0];
+        lastOption = HTMLSuggestions[HTMLSuggestions.length - 1];
+        toggleHighlight(firstOption)
+    });
+
+    suggestionDisplay.addEventListener('click', (event) => {
+        searchBar.value = event.target.textContent;
+        suggestions = [];
+        displaySuggestions();
+    });
+
+    suggestionDisplay.addEventListener('mouseover', (event) => {
+        if (event.target !== firstOption) {
+            toggleHighlight(event.target);
+            if (firstOption.id === 'highlight') {
+                toggleHighlight(firstOption);
+            }
+        } else if (firstOption.id !== 'highlight') {
+            toggleHighlight(firstOption);
+        }
+    });
+
+    suggestionDisplay.addEventListener('mouseout', (event) => {
+        toggleHighlight(event.target);
+    });
+
+    searchBar.addEventListener('keydown', (event) => {
+        const previousOption = document.getElementById('highlight');
+        if (event.keyCode === 40) {
+            event.preventDefault();
+            toggleHighlight(previousOption);
+            previousOption.nextSibling !== null ? toggleHighlight(previousOption.nextSibling) : toggleHighlight(firstOption);
+        } else if (event.keyCode === 38) {
+            event.preventDefault();
+            toggleHighlight(previousOption);
+            previousOption.previousSibling !== null ? toggleHighlight(previousOption.previousSibling) : toggleHighlight(lastOption);
+        } else if (event.keyCode === 13) {
+            event.preventDefault();
+            searchBar.value = previousOption.textContent;
+        }
+    });
+}
+
+function displaySuggestions() {
+    document.getElementById('suggestions').innerHTML = (suggestions.map(element => '<li class=\'suggestion\'>'+element+'</li>')).join('');
+}
+
+function toggleHighlight(HTMLElement) {
+    HTMLElement.id === 'highlight' ? HTMLElement.id = null : HTMLElement.id = 'highlight';
+}
